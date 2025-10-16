a8bfb19e1c5c0222ba282c4ed46ade9116253b63
Rahul14055 <pokego6361676632@gmail.com>
2025-10-16 11:25:28 +0530

    Update CB2.py

--- CB2.py
+++ CB2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Simple Command App")
+st.title("Simple Chatbot")
 
 st.session_state.messages = []
 
@@ -17,3 +17,4 @@ if st.button("Run Command"):
 st.subheader("Output:")
 for msg in st.session_state.messages:
     st.text(msg)
+
